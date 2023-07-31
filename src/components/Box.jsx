1530fc4087ddc0163b4573af05edd432a5530419
dennisleeNCCUDCT <95065965+dennisleeNCCUDCT@users.noreply.github.com>
2023-07-31 20:45:36 +0800

    update

--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import { useRef } from 'react'
 import { useEffect } from 'react'
-import { useFrame } from '@react-three/fiber'
+//import { useFrame } from '@react-three/fiber'
+import { red } from 'react-color/lib/helpers/color'
 
 const Box = (props) => {
   const ref = useRef()
@@ -11,21 +12,25 @@ const Box = (props) => {
   })
 
   //
-  useFrame((_, delta) => {
-    ref.current.rotation.x += 1 * delta
-    ref.current.rotation.y += 0.5 * delta
-    ref.current.position.z -= 0.5 * delta
-    //
-  })
+  //useFrame((_, delta) => {
+  //ref.current.rotation.x += 1 * delta
+  //ref.current.rotation.y += 0.5 * delta
+  // ref.current.position.z -= 0.5 * delta
   //
-  useFrame(({ clock }) => {
-    ref.current.position.y = Math.sin(clock.getElapsedTime() * 5)
-  })
+  // })
+  //
+  // useFrame(({ clock }) => {
+  // ref.current.position.y = Math.sin(clock.getElapsedTime() * 5)
+  // })
   //
   return (
-    <mesh {...props} ref={ref}>
+    <mesh
+      {...props}
+      ref={ref}
+      onPointerDown={(e) => (ref.current.position.y += 1)}
+      onPointerUp={(e) => (ref.current.position.y -= 1)}>
       <boxGeometry />
-      <meshBasicMaterial color={0x00ff00} wireframe />
+      <meshBasicMaterial color={red} wireframe />
     </mesh>
   )
 }
